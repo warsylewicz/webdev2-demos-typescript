e16a0d103ad3d758c49fe6b41623d76d62192c47
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-11-30 07:14:44 -0700

    Add explanations

--- app/week-13/custom-solutions/page.tsx
+++ app/week-13/custom-solutions/page.tsx
@@ -3,17 +3,12 @@ export default function Page() {
     <div>
       <h1>Custom Solutions</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        tincidunt, ex nec ultrices aliquam, ipsum urna aliquet nisi, eget
-        ultricies nunc arcu eget est. Donec sed lectus euismod, viverra ligula
-        quis, ultrices nunc. Sed vitae nisl euismod, finibus nisl vitae, aliquet
-        nisl. Donec auctor, nisl in ultrices tincidunt, nisl lacus aliquam quam,
-        non dignissim eros nisi quis dolor. Donec tincidunt, ex nec ultrices
-        aliquam, ipsum urna aliquet nisi, eget ultricies nunc arcu eget est.
-        Donec sed lectus euismod, viverra ligula quis, ultrices nunc. Sed vitae
-        nisl euismod, finibus nisl vitae, aliquet nisl. Donec auctor, nisl in
-        ultrices tincidunt, nisl lacus aliquam quam, non dignissim eros nisi
-        quis dolor.
+        This page used to be called "Sales", but your boss decided to change the
+        name to "Custom Solutions" because it sounds more professional. However,
+        because of this change, the URL changed as well. You need to make sure
+        that the old URL (/sales) still works and redirects to the new otherwise
+        you will lose a lot of customers. Notice how `middleware.tsx` is used to
+        redirect the user to the new URL.
       </p>
     </div>
   );
--- app/week-13/really-long-url-that-is-hard-to-remember/page.tsx
+++ app/week-13/really-long-url-that-is-hard-to-remember/page.tsx
@@ -3,17 +3,10 @@ export default function Page() {
     <div>
       <h1>Really Long URL that is Hard to Remember</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        tincidunt, ex nec ultrices aliquam, ipsum urna aliquet nisi, eget
-        ultricies nunc arcu eget est. Donec sed lectus euismod, viverra ligula
-        quis, ultrices nunc. Sed vitae nisl euismod, finibus nisl vitae, aliquet
-        nisl. Donec auctor, nisl in ultrices tincidunt, nisl lacus aliquam quam,
-        non dignissim eros nisi quis dolor. Donec tincidunt, ex nec ultrices
-        aliquam, ipsum urna aliquet nisi, eget ultricies nunc arcu eget est.
-        Donec sed lectus euismod, viverra ligula quis, ultrices nunc. Sed vitae
-        nisl euismod, finibus nisl vitae, aliquet nisl. Donec auctor, nisl in
-        ultrices tincidunt, nisl lacus aliquam quam, non dignissim eros nisi
-        quis dolor.
+        The URL for this page is really long and hard to remember. The
+        middleware.tsx rewrites a shorter URL to this page. Try going to both
+        /week-13/really-long-url-that-is-hard-to-remember and /week13/short-url
+        to see how it works.
       </p>
     </div>
   );
